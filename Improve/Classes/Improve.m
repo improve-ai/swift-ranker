7da8cb278a64c77763f56b8a19c6857ba60cede3
Natalie Chapweske <natz@chapweske.com>
2016-12-15 18:52:25 -0700

    Removed remote loading Removed unused "sort" type

--- Improve/Classes/Improve.m
+++ Improve/Classes/Improve.m
@@ -9,13 +9,10 @@
 #import <Foundation/Foundation.h>
 #import "Improve.h"
 
-#define SORT_TYPE @"sort"
-
 #define CHOOSE_URL @"https://api.improve.ai/v1/choose"
 #define TRACK_URL @"https://api.improve.ai/v1/track"
 
 #define USER_ID_KEY @"ai.improve.user_id"
-
 
 @implementation Improve : NSObject
 
@@ -59,17 +56,6 @@ static Improve *sharedInstance;
     [self chooseFrom:choices prices:nil forKey:key funnel:funnel sort:false block:block];
 }
 
-- (void)chooseFromRemote:(NSURLRequest *)choicesRequest forKey:(NSString *)key funnel:(NSArray *)funnel block:(void (^)(NSObject *, NSError *)) block
-{
-    [self fetchRemoteArray:choicesRequest block:^(NSArray *result, NSError *error) {
-        if (result && !error) {
-            [self chooseFrom:result forKey:key funnel:funnel block:block];
-        } else {
-            block(nil,error);
-        }
-    }];
-}
-
 - (void)choosePriceFrom:(NSArray *)prices forKey:(NSString *)key funnel:(NSArray *)funnel block:(void (^)(NSNumber *, NSError *)) block
 {
     [self chooseFrom:nil prices:prices forKey:key funnel:funnel sort:false block:^(NSObject *result, NSError *error) {
@@ -80,17 +66,6 @@ static Improve *sharedInstance;
         }
     }];
 
-}
-
-- (void)choosePriceFromRemote:(NSURLRequest *)pricesRequest forKey:(NSString *)key funnel:(NSArray *)funnel block:(void (^)(NSNumber *, NSError *)) block
-{
-    [self fetchRemoteArray:pricesRequest block:^(NSArray *result, NSError *error) {
-        if (result && !error) {
-            [self choosePriceFrom:result forKey:key funnel:funnel block:block];
-        } else {
-            block(nil,error);
-        }
-    }];
 }
 
 - (void)sort:(NSArray *)choices forKey:(NSString *)key funnel:(NSArray *)funnel block:(void (^)(NSArray *, NSError *)) block
@@ -104,16 +79,6 @@ static Improve *sharedInstance;
     }];
 }
 
-- (void)sortFromRemote:(NSURLRequest *)choicesRequest forKey:(NSString *)key funnel:(NSArray *)funnel block:(void (^)(NSArray *, NSError *)) block
-{
-    [self fetchRemoteArray:choicesRequest block:^(NSArray *result, NSError *error) {
-        if (result && !error) {
-            [self sort:result forKey:key funnel:funnel block:block];
-        } else {
-            block(nil,error);
-        }
-    }];
-}
 
 - (void)chooseFrom:(NSArray *)choices prices:(NSArray *)prices forKey:(NSString *)key funnel:(NSArray *)funnel sort:(BOOL)sort block:(void (^)(NSObject *, NSError *)) block
 {
@@ -161,18 +126,6 @@ static Improve *sharedInstance;
     }];
 }
 
-- (void) fetchRemoteArray:(NSURLRequest *) request block:(void (^)(NSArray *, NSError *)) block
-{
-    [self fetchJson:request block:^(NSObject *response, NSError *error) {
-        if (error) {
-            block(nil, error);
-        } else if (![response isKindOfClass:[NSArray class]]) {
-            block(nil, [NSError errorWithDomain:@"ai.improve" code:400 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"expected array, got %@", response]}]);
-        } else {
-            block(response, nil);
-        }
-    }];
-}
 
 - (void) postImproveRequest:(NSString *) url body:(NSDictionary *) body block:(void (^)(NSObject *, NSError *)) block
 {
@@ -188,14 +141,6 @@ static Improve *sharedInstance;
     [request setAllHTTPHeaderFields:headers];
     [request setHTTPBody:postData];
     
-    [self fetchJson:request block:block];
-}
-
-/*
- Fetch and parse the JSON response accessible via the NSURLRequest.  Since the caller sets up the requests it could be a full blown POST or a simple GET.
- The callback is executed on the main thread.
- */
-- (void) fetchJson:(NSURLRequest *) request block:(void (^)(NSObject *, NSError *)) block {
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
                                                           delegate:nil
                                                      delegateQueue:[NSOperationQueue mainQueue]];
