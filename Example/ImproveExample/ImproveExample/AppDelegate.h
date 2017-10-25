78b61436cd4d7cc153527dc36320dcd6a536d6bf
Zeljko <zzivkovi@gmail.com>
2017-10-25 14:11:53 +0200

    added example project

---
+++ Example/ImproveExample/ImproveExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImproveExample
+//
+//  Created by Zeljko Zivkovic on 25/10/2017.
+//  Copyright © 2017 Improve.ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/ImproveExample/ImproveExample/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  ImproveExample
+//
+//  Created by Zeljko Zivkovic on 25/10/2017.
+//  Copyright © 2017 Improve.ai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)improveTestSelected:(id)sender
+{
+	NSLog(@"Test");
+}
+
+@end
