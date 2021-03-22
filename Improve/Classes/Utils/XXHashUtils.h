9aedf734361315569667342205d678deeeb6c371
Hongxi Pan <panhxi@gmail.com>
2021-03-22 14:07:15 +0800

    add xxhash encoding utils for filename encoding

---
+++ Improve/Classes/Utils/XXHashUtils.h
@@ -0,0 +1,19 @@
+//
+//  XXHashUtils.h
+//  ImproveUnitTests
+//
+//  Created by PanHongxi on 3/22/21.
+//  Copyright © 2021 Mind Blown Apps, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XXHashUtils : NSObject
+
++ (NSString *)encode:(NSString *)string;
+
+@end
+
+NS_ASSUME_NONNULL_END
