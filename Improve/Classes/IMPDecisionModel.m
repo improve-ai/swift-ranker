e21dfe2b6cd2c5408464a93e02b0b8ca67837220
Hongxi Pan <panhxi@gmail.com>
2021-08-05 11:53:44 +0800

    scale tie-breaker random number by 2**-23 so they fall outside 32 bit precision

--- Improve/Classes/IMPDecisionModel.m
+++ Improve/Classes/IMPDecisionModel.m
@@ -213,7 +213,7 @@
         NSMutableArray *scores = [NSMutableArray arrayWithCapacity:prediction.count];
         for (NSUInteger i = 0; i < prediction.count; i++) {
             double val = [[prediction featuresAtIndex:i] featureValueForName:@"target"].doubleValue;
-            val += ((double)arc4random() / UINT32_MAX) * pow(2, -17); // add a very small random number to randomly break ties
+            val += ((double)arc4random() / UINT32_MAX) * pow(2, -23); // add a very small random number to randomly break ties
             [scores addObject:@(val)];
         }
         return scores;
