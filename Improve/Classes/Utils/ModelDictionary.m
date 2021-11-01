8daa210d883b27d1e03e02234067b67bebc4a869
Hongxi Pan <panhxi@gmail.com>
2021-11-01 19:48:04 +0800

    Thread safe ModelDictionary

--- Improve/Classes/Utils/ModelDictionary.m
+++ Improve/Classes/Utils/ModelDictionary.m
@@ -13,42 +13,54 @@
 
 @property (strong, nonatomic) NSMutableDictionary<NSString *, IMPDecisionModel *> *models;
 
+@property (strong, nonatomic) dispatch_queue_t readWriteQueue;
+
 @end
 
 @implementation ModelDictionary
 
-- (NSMutableDictionary<NSString *, IMPDecisionModel *> *)models {
-    @synchronized (self) {
-        if(_models == nil) {
-            _models = [[NSMutableDictionary alloc] init];
-        }
+- (instancetype)init {
+    if(self = [super init]) {
+        _models = [[NSMutableDictionary alloc] init];
+        _readWriteQueue = dispatch_queue_create("ai.improve.models", DISPATCH_QUEUE_CONCURRENT);
     }
-    return _models;
+    return self;
 }
 
 - (IMPDecisionModel *)objectForKeyedSubscript:(NSString *)modelName {
-    IMPDecisionModel *model = self.models[modelName];
-    if(model == nil) {
-        model = [[IMPDecisionModel alloc] initWithModelName:modelName];
-        self.models[modelName] = model;
-    }
+    __block IMPDecisionModel *model = nil;
+    dispatch_sync(self.readWriteQueue, ^{
+        model = self.models[modelName];
+        if(model == nil) {
+            model = [[IMPDecisionModel alloc] initWithModelName:modelName];
+            self.models[modelName] = model;
+        }
+    });
     return model;
 }
 
 - (void)setObject:(IMPDecisionModel *)model forKeyedSubscript:(NSString *)modelName {
-    if(model != nil && ![model.modelName isEqualToString:modelName]) {
-        NSString *reason = [NSString stringWithFormat:@"modelName(%@) must be equal to model.modelName(%@)", modelName, model.modelName];
-        @throw [NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo:nil];
-    }
-    self.models[modelName] = model;
+    dispatch_barrier_async(self.readWriteQueue, ^{
+        if(model != nil && ![model.modelName isEqualToString:modelName]) {
+            NSString *reason = [NSString stringWithFormat:@"modelName(%@) must be equal to model.modelName(%@)", modelName, model.modelName];
+            @throw [NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo:nil];
+        }
+        self.models[modelName] = model;
+    });
 }
 
 - (NSUInteger)count {
-    return [self.models count];
+    __block NSUInteger c = 0;
+    dispatch_sync(self.readWriteQueue, ^{
+        c = [self.models count];
+    });
+    return c;
 }
 
 - (void)clear {
-    [self.models removeAllObjects];
+    dispatch_barrier_async(self.readWriteQueue, ^{
+        [self.models removeAllObjects];
+    });
 }
 
 @end
