899ffc0ca8c5a465a404bac6d2ee9d8c9eff0c0b
Hongxi Pan <panhxi@gmail.com>
2022-04-22 13:22:54 +0800

    decode chosen variant in decision.get()

--- ImproveAISwift/ImproveAI.swift
+++ ImproveAISwift/ImproveAI.swift
@@ -4,4 +4,22 @@ extension DecisionModel {
     public static subscript(modelName: String) -> DecisionModel {
         return DecisionModel.instances[modelName]
     }
+    
+    public func chooseFrom<T : Codable>(variants: [T]) -> Decision {
+        let encoder = JSONEncoder()
+        let encodedVariants = variants.map ({ (variant) -> Dictionary<String, Any> in
+            let data = try? encoder.encode(variant)
+            return try! JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! Dictionary<String, Any>
+        })
+        return chooseFrom(encodedVariants)
+    }
+}
+
+extension Decision {
+    public func get<T: Codable>() -> T {
+        let decoder = JSONDecoder.init()
+        let chosenVariant = get()
+        let data = try? JSONSerialization.data(withJSONObject: chosenVariant, options: .prettyPrinted)
+        return try! decoder.decode(T.self, from: data!)
+    }
 }
