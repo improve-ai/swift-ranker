6b84d036cc3129536d4a1def067c5f24eef6fd64
Hongxi Pan <panhxi@gmail.com>
2022-06-07 15:18:49 +0800

    generate doc with jazzy

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-appledoc --project-name ImproveAI --project-company ImproveAI --no-create-docset --no-install-docset --no-publish-docset .
-cp -R html docs
+sourcekitten doc --objc ImproveAI/include/ImproveAI/ImproveAI.h -- -x objective-c -isysroot $(xcrun --show-sdk-path) -I$(pwd) > core.json
+sourcekitten doc --single-file ImproveAISwift/ImproveAI.swift -- -j4 ImproveAISwift/ImproveAI.swift > swift.json
+jazzy \
+    --sdk iphoneos \
+    --author ImproveAI \
+    --author_url https://improve.ai \
+    --source-host github \
+    --source-host-url https://github.com/improve-ai/ios-sdk \
+    --theme apple \
+    --sourcekitten-sourcefile core.json,swift.json
+
+rm core.json swift.json
