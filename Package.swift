aff7532b111812f82cb8ce556635fda2be883056
Justin <justin@mindblownapp.com>
2023-06-24 20:01:20 -0600

    8.0.2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let improveAIVersion = "8.0.1"
+let improveAIVersion = "8.0.2"
 
 let package = Package(
     name: "ImproveAI",
